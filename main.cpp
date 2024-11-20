382ba5eaf136d3dc07cb7e479ea1dfbc431403b4
thehilmisu <ozcanhakanzi@gmail.com>
2024-11-20 19:02:21 +0100

    nvim config from arch-laptop

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iterator>
+
+int main() {
+  int x = 0;
+  int a = 3;
+  std::cout << "jalal" << std::endl;
+  if (x == 5) {
+    std::cout << "x :" << std::endl;
+  }
+
+  float vector = 0.01f;
+
+  int s = 4;
+}
